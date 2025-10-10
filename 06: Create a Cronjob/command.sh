be69da78f2ab5392d2aa7d270d5dd13aca38ad96
maulanamaky <159880655+maulanamaky@users.noreply.github.com>
2025-10-10 13:47:44 +0700

    Create command.sh

---
+++ 06: Create a Cronjob/command.sh
@@ -0,0 +1,7 @@
+# TBH, i forgot about to note the questions.
+# Do SSH on each server
+# 1. Install cron server
+# 2. Make sure cron server has running
+# 3. Make a new file to task and use cron for automation
+crontab -e
+
