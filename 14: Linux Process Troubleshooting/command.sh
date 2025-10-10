36d32babc5a064833abc82a91ff608330f44ec6b
maulanamaky <159880655+maulanamaky@users.noreply.github.com>
2025-10-10 13:51:53 +0700

    Create command.sh

---
+++ 14: Linux Process Troubleshooting/command.sh
@@ -0,0 +1,14 @@
+# Login to App server which is trouble
+
+sudo systemctl status httpd
+
+sudo netstat -tulpn | grep port_number
+
+# You can see port_number has take for another service
+# We can stop that service, and run again for httpd
+# fill service_name with the real service who has same port with httpd
+
+sudo systemctl stop service_name
+sudo systemctl disable service_name
+
+sudo systemctl start httpd
