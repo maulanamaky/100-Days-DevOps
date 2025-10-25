d092f7f736745484593f6cca292d9d0ed2c4f892
maulanamaky <159880655+maulanamaky@users.noreply.github.com>
2025-10-25 14:52:56 +0700

    Update command.sh

--- 55: Kubernetes Sidecar Containers/command.sh
+++ 55: Kubernetes Sidecar Containers/command.sh
@@ -1,1 +1,7 @@
+# 1. Make pod imperative and using --dry-run and edit
+kubectl run webserver --image nginx:latest --dry-run=client -o yaml > pod.yaml
 
+vi pod.yaml
+
+# 2. Deplu
+kubectl apply -f pod.yaml
